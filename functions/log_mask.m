b8414f35ceec388dc3083c6109380c7b7c7c3e55
ozer0532 <ozer0532@gmail.com>
2022-03-28 09:36:33 +0700

    🐛 Fix LoG mask not working on Matlab
    Matlab kinda cringe tho

--- functions/log_mask.m
+++ functions/log_mask.m
@@ -3,7 +3,12 @@ function [mask] = log_mask(size, std)
         error("mask size should be an odd number")
     end
 
-    halfsize = int32(floor(size/2))
-    [X, Y] = meshgrid(-halfsize:halfsize, -halfsize:halfsize)
-    mask = ((X.^2 + Y.^2 - 2*std^2)/(std^4)) .* exp(-(X.^2 + Y.^2)/(2*std^2))
+    if(size < 5)
+        error("mask size should be at least 5")
+    end
+
+    halfsize = int32(floor(size/2));
+    [X, Y] = meshgrid(-halfsize:halfsize, -halfsize:halfsize);
+    X = double(X); Y = double(Y);
+    mask = ((X.^2 + Y.^2 - 2*std^2)./(std^4)) .* exp(-(X.^2 + Y.^2)./(2*std^2));
 end
